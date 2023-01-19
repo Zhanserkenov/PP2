56abb719adbb05072368a520d54ea3bdf43cb3b6
Zhanserkenov Arsen <73212575+Zhanserkenov@users.noreply.github.com>
2023-01-19 21:13:31 +0600

    Data Types

---
+++ week1/PYTHON_Data_Types.py
@@ -0,0 +1,41 @@
+#Exercise 1
+
+x = 5
+print(type(x))
+#data type of x int
+
+#Exercise 2
+
+x = "Hello World"
+print(type(x))
+#data type of x - str
+
+#Exercise 3
+
+x = 20.5
+print(type(x))
+#data type of x - float
+
+#Exercise 4
+
+x = ["apple", "banana", "cherry"]
+print(type(x))
+#data type of x - list
+
+#Exercise 5
+
+x = ("apple", "banana", "cherry")
+print(type(x))
+#data type of x - tuple
+
+#Exercise 6
+
+x = {"name" : "John", "age" : 36}
+print(type(x))
+#data type of x - dict
+
+#Exercise 7
+
+x = True
+print(type(x))
+#data type of x - bool
